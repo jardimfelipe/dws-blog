924475ef5de55f4e6dfaee87ef8622c563835a1d
Felipe Jardim <jardim.felipe@hotmail.com>
2024-07-27 20:14:14 -0300

    chore: install react query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import Root from "./routes/root.tsx";
 import Error from "./routes/error.tsx";
+import Posts from "./routes/posts.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <Error />,
+    children: [
+      {
+        path: "/posts",
+        element: <Posts />,
+      },
+    ],
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/routes/posts.tsx
@@ -0,0 +1,13 @@
+import usePostsQuery from "../modules/post/usePostsQuery";
+
+export default function Posts() {
+  const { data = [] } = usePostsQuery();
+  return (
+    <div>
+      posts:
+      {data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+}
